0161287fbaaad9b207da803aa1403e135edef6c2
David Poole <dbpoole7@gmail.com>
2024-02-16 23:54:41 -0600

    initial setup and user authentication completed

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function Home() {
       }}
     >
       <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
+      <p>Your Bio: {user.name}</p>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
